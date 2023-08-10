f434659de92fb0f75fbd680d8a4862595f883127
khanice <86192244+khanice@users.noreply.github.com>
2023-08-10 15:12:45 +0530

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,31 @@
+import "./styles.css";
+var btnw = document.getElementById('Clicked');
+btnw.addEventListener('click',()=>{
+  move();
+});
+
+
+var i=0;
+function move(){
+  if(i===0){
+    i=1;
+    var elem = document.getElementById("mybar");
+    var width = 10;
+    var id = setInterval(frame,10);
+    function frame(){
+      if(width>=100){
+        clearInterval(id);
+        i=0;
+      }else{
+        width++;
+        elem.style.width = width +"%";
+        elem.innerHTML = width + "%";
+      }
+    }
+
+  }
+}
+
+
+
+
